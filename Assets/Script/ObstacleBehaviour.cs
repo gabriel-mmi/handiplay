7e4ddc670d027c2b98105f67fd92a2f6a5a7436b
NicoooM <nmarsan33000@gmail.com>
2021-03-31 18:00:57 +0200

    obstacle behaviour

---
+++ Assets/Script/ObstacleBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehaviour : MonoBehaviour
+{
+
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        speed = Random.Range(4, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = transform.localPosition + Time.deltaTime * speed * Vector3.right;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.tag == "Limit")
+        //{
+        //    transform.Rotate(0, 180, 0);
+        //}
+
+        //if (collision.gameObject.tag == "Player")
+        //{
+        //    die();
+        //}
+    }
+}
