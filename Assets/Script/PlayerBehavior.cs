334e650de83c3f48eb2d77d0b869ceafaf076f4b
k4y0oyak <kayomanjos@gmail.com>
2021-03-31 16:44:34 +0200

    jump mechanic

--- Assets/Script/PlayerBehavior.cs
+++ Assets/Script/PlayerBehavior.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    public float jumpForce;
+
+    private Rigidbody rb;
+
+    private bool isGrounded = false;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatisGrounded;
+
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(feetPos.position, checkRadius, whatisGrounded);
         
+        if (isGrounded==true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector3.up * jumpForce;
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if(jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector3.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;  
+            } else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+    }
+
+    void Jump()
+    {
+
     }
 }
